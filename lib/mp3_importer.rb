b13161353e76e88a93b9528b4f56f5cf54a04f00
Allison Ricker <alliricker@gmail.com>
2020-10-16 02:44:30 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,8 +11,10 @@ def files
 end
 
 def import
-  self.files.each { |file| Song.new_by_filename(file)
-  Artist.all << song.artist unless Artist.all.include?(song.artist) }
+  self.files.each do |file|
+  Song.new_by_filename(file)
+  Artist.all << song.artist unless Artist.all.include?(song.artist)
+end
 end
 
 end
