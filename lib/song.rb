b808194d3f4e69cc77cedfe3537df659af79d54a
Allison Ricker <alliricker@gmail.com>
2020-10-16 02:36:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,8 @@ def self.all
 end
 
 def self.new_by_filename(filename)
-  splitname = filename.split(" - ")
-  song = self.new(splitname[1])
+  split_name = filename.split(" - ")
+  song = self.new(split_name[1])
   artist = Artist.find_or_create_by_name(split_name[1])
   song.artist = artist
   artist.add_song(song)
